2eeb5287f0755fb17d360b35594c23fac04d4e92
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-01-11 13:06:50 -0600

    explicit path to vault pw

--- scripts/configure-jitsi-repo.sh
+++ scripts/configure-jitsi-repo.sh
@@ -2,4 +2,4 @@
 
 # e.g. ../all/bin/terraform/standalone
 
-ansible-playbook -v -i "127.0.0.1," -c local ansible/configure-jitsi-repo.yml
+ansible-playbook -v -i "127.0.0.1," -c local ansible/configure-jitsi-repo.yml --vault-password-file .vault-password.txt
