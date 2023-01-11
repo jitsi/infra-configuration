47adaaf207e5e0a1adbf6e72328a53df741f6ce3
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-01-11 12:53:10 -0600

    no more relative paths for configure jitsi repo

--- scripts/configure-jitsi-repo.sh
+++ scripts/configure-jitsi-repo.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # e.g. ../all/bin/terraform/standalone
-LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
 
-ansible-playbook -i "127.0.0.1," -c local "$LOCAL_PATH/../ansible/configure-jitsi-repo.yml"
+ansible-playbook -i "127.0.0.1," -c local ansible/configure-jitsi-repo.yml
