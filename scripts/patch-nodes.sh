79d3e90ddaf291cb01f0a52a53c0a11e85d60fbc
scott boone <sawall@users.noreply.github.com>
2023-02-10 12:58:40 -0600

    feature: patch batch takes an environment list (#44)

--- scripts/patch-nodes.sh
+++ scripts/patch-nodes.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#IF THE CURRENT DIRECTORY HAS stack-env.sh THEN INCLUDE IT
-[ -e ./stack-env.sh ] && . ./stack-env.sh
-
 echo "## starting patch-nodes.sh"
 
 if [  -z "$1" ]
@@ -14,16 +11,26 @@ else
   echo "Run ansible as $ANSIBLE_SSH_USER"
 fi
 
-if [ -z "$ENVIRONMENT" ]; then
-  echo "## ERROR in patch-nodes.sh: ENVIRONMENT must be set"
-  exit 2
+if [ -z "$ENVIRONMENT_LIST" ]; then
+    if [ -z "$ENVIRONMENT" ]; then
+        ENVIRONMENT_LIST=$ENVIRONMENT
+    else
+        echo "## ERROR in patch-nodes.sh: ENVIRONMENT or ENVIRONMENT_LIST must be set"
+        exit 2
+    fi
+fi
+
+LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
+
+if [[ "$ENVIRONMENT_LIST" == "ALL" ]]; then
+    ENVIRONMENT_LIST=$(ls $LOCAL_PATH/../sites/)
+    echo "## applying patch to ALL environments: $ENVIRONMENT_LIST"
+else
+    echo "## applying patch to these environments: $ENVIRONMENT_LIST"
 fi
 
 [ -z "$ROLE" ] && ROLE="ssh"
 [ -z "$ORACLE_REGION" ] && ORACLE_REGION="all"
-
-LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
-[ -e $LOCAL_PATH/../sites/$ENVIRONMENT/stack-env.sh ] && . $LOCAL_PATH/../sites/$ENVIRONMENT/stack-env.sh
 
 RELEASE_PARAM=""
 if [ -n "$RELEASE_NUMBER" ]; then 
@@ -31,12 +38,12 @@ if [ -n "$RELEASE_NUMBER" ]; then
     RELEASE_PARAM="--release ${RELEASE_NUMBER}"
 fi
 
-if [ -z "$ANSIBLE_INVENTORY" ]; then 
-  ANSIBLE_INVENTORY="./batch-${ROLE}-${ORACLE_REGION}.inventory"
-  $LOCAL_PATH/node.py --environment $ENVIRONMENT --role $ROLE --region $ORACLE_REGION --oracle --batch --inventory $RELEASE_PARAM > $ANSIBLE_INVENTORY
-fi
+rm -rf ./batch-${ROLE}-${ORACLE_REGION}*.inventory
 
-set -x
+for ENV in $ENVIRONMENT_LIST; do
+  ANSIBLE_INVENTORY="./batch-${ROLE}-${ORACLE_REGION}-${ENV}.inventory"
+  $LOCAL_PATH/node.py --environment $ENV --role $ROLE --region $ORACLE_REGION --oracle --batch --inventory $RELEASE_PARAM > $ANSIBLE_INVENTORY
+done
 
 DEPLOY_TAGS=${ANSIBLE_TAGS-"all"}
 
@@ -54,37 +61,44 @@ BATCH_SIZE=${BATCH_SIZE-"10"}
 
 [ -d ./.batch ] && rm -rf .batch
 mkdir .batch
-split -l $BATCH_SIZE $ANSIBLE_INVENTORY ".batch/${ROLE}-${ORACLE_REGION}-"
+for ENV in $ENVIRONMENT_LIST; do
+    ANSIBLE_INVENTORY="./batch-${ROLE}-${ORACLE_REGION}-${ENV}.inventory"
+    split -l $BATCH_SIZE $ANSIBLE_INVENTORY ".batch/${ROLE}-${ORACLE_REGION}-${ENV}-"
+done
 
 FAILED_COUNT=0
 ANSIBLE_FAILURES=0
 
-for BATCH_INVENTORY in .batch/${ROLE}-${ORACLE_REGION}-*; do
-    echo "[tag_shard_role_$ROLE]" > ./batch.inventory
-    if [[ "$SKIP_SSH_CONFIRMATION" == "true" ]]; then
-        cat $BATCH_INVENTORY >> ./batch.inventory
-    else
-        for ip in $(cat $BATCH_INVENTORY | tail -n+1 | awk '{print $1}'); do
-            timeout 10 ssh -o StrictHostKeyChecking=no -F $LOCAL_PATH/../config/ssh.config $ANSIBLE_SSH_USER@$ip "uptime > /dev/null" && echo $ip >> ./batch.inventory || FAILED_COUNT=$(($FAILED_COUNT+1))
-        done
-    fi
+set -x
 
-    LIVE_COUNT=$(cat ./batch.inventory | wc -l | awk '{print $1}')
-    if [[ $LIVE_COUNT -gt 1 ]]; then
-
-        ansible-playbook $ANSIBLE_PLAYBOOK \
-            -i ./batch.inventory \
-            -e "ansible_ssh_user=$ANSIBLE_SSH_USER hcv_environment=$ENVIRONMENT shard_role=$ROLE patch_ansible_roles=\"$ANSIBLE_ROLES\"" \
-            $ANSIBLE_EXTRA_VARS --vault-password-file .vault-password.txt \
-            --tags "$DEPLOY_TAGS"
-
-        if [[ $? -gt 0 ]]; then
-            echo "ERROR: Ansible batch failed"
-            ANSIBLE_FAILURES=$(($ANSIBLE_FAILURES+1))
+for ENV in $ENVIRONMENT_LIST; do
+    for BATCH_INVENTORY in .batch/${ROLE}-${ORACLE_REGION}-${ENV}*; do
+        echo "[tag_shard_role_$ROLE]" > ./batch.inventory
+        if [[ "$SKIP_SSH_CONFIRMATION" == "true" ]]; then
+            cat $BATCH_INVENTORY >> ./batch.inventory
+        else
+            for ip in $(cat $BATCH_INVENTORY | tail -n+1 | awk '{print $1}'); do
+                timeout 10 ssh -o StrictHostKeyChecking=no -F $LOCAL_PATH/../config/ssh.config $ANSIBLE_SSH_USER@$ip "uptime > /dev/null" && echo $ip >> ./batch.inventory || FAILED_COUNT=$(($FAILED_COUNT+1))
+            done
         fi
-    else
-        echo "No live instances found in batch, skipping"
-    fi
+
+        LIVE_COUNT=$(cat ./batch.inventory | wc -l | awk '{print $1}')
+        if [[ $LIVE_COUNT -gt 1 ]]; then
+
+            ansible-playbook $ANSIBLE_PLAYBOOK \
+                -i ./batch.inventory \
+                -e "ansible_ssh_user=$ANSIBLE_SSH_USER hcv_environment=$ENV shard_role=$ROLE patch_ansible_roles=\"$ANSIBLE_ROLES\"" \
+                $ANSIBLE_EXTRA_VARS --vault-password-file .vault-password.txt \
+                --tags "$DEPLOY_TAGS"
+
+            if [[ $? -gt 0 ]]; then
+                echo "ERROR: Ansible batch failed for ${ENV}"
+                ANSIBLE_FAILURES=$(($ANSIBLE_FAILURES+1))
+            fi
+        else
+            echo "No live instances found in batch for ${ENV}, skipping"
+        fi
+    done
 done
 
 FINAL_RET=0
