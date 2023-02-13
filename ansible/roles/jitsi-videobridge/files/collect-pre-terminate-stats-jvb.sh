7b9730147addf5cba0f5619b4d0a5f3821dcf9b5
bgrozev <boris@jitsi.org>
2023-02-13 16:07:29 -0600

    Add scripts to collects jvb/jicofo stats pre-termination. (#31)

---
+++ ansible/roles/jitsi-videobridge/files/collect-pre-terminate-stats-jvb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x
+
+[ -z "$COLLECTION_FILENAME" ] && COLLECTION_FILENAME="$(mktemp -d)/jvb-stats.tar.gz"
+echo "Will collect stats in $COLLECTION_FILENAME"
+
+DIR=$(mktemp -d)
+
+cd $DIR
+
+for i in payload-verification node-stats pool-stats queue-stats transit-stats task-pool-stats \
+  ice-stats xmpp-delay-stats tossed-packet-stats; do
+    curl -s 0:8080/debug/stats/jvb/$i | jq . > $i.json
+done
+
+curl -s 0:8080/colibri/stats | jq . > colibri-stats.json
+curl -s 0:8080/metrics | jq . > metrics.json
+
+tar czf jvb-stats.tar.gz *
+cd -
+mv "${DIR}/jvb-stats.tar.gz" "$COLLECTION_FILENAME"
