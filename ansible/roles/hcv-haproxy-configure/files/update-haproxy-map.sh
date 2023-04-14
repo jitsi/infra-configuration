d44fd113427f0d8e725b8a1d1aafe15b98c9997e
Scott Boone <scott.boone@8x8.com>
2023-04-14 16:11:11 -0500

    script updates

--- ansible/roles/hcv-haproxy-configure/files/update-haproxy-map.sh
+++ ansible/roles/hcv-haproxy-configure/files/update-haproxy-map.sh
@@ -2,15 +2,23 @@
 #
 # read a haproxy map file and uses haproxy admin socket to update the live configuration
 
+echo "## starting update-haproxy-map.sh"
+
+if [ -n "$1" ]; then
+    UPDATE_MAP=$1
+fi
+
 if [ -z "$UPDATE_MAP" ]; then
-  echo "no UPDATE_MAP found, exiting..."
+  echo "## update-haproxy-map.sh: no UPDATE_MAP found, exiting..."
   exit 1
 fi
 
 if [ ! -f "$UPDATE_MAP" ]; then
-    echo -e "map file ${UPDATE_MAP} does not exist"
+    echo -e "## update-haproxy-map.sh: map file ${UPDATE_MAP} does not exist"
     exit 1 
 fi
+
+echo "## updating live config with $UPDATE_MAP"
 
 while IFS= read -r line; do
     echo "set map $UPDATE_MAP $line" | socat /var/run/haproxy/admin.sock stdio
