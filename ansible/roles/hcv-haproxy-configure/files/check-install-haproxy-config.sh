51cfe16f5fae70f55bb50435fa0f1f8057cb8cef
scott boone <scott.e.boone@gmail.com>
2023-04-24 11:20:39 -0700

    log streamline

--- ansible/roles/hcv-haproxy-configure/files/check-install-haproxy-config.sh
+++ ansible/roles/hcv-haproxy-configure/files/check-install-haproxy-config.sh
@@ -2,35 +2,30 @@
 #
 # check a draft haproxy config and install if it's valid
 
-function logit() {
-  echo "## $1"
-  logger -p local0.notice -t ${0##*/}[$$] "$1"
-}
-
-logit "starting check-install-haproxy-config.sh"
+echo "starting check-install-haproxy-config.sh"
 
 if [ -n "$1" ]; then
     DRAFT_CONFIG=$1
 fi
 
 if [ -z "$DRAFT_CONFIG" ]; then
-  logit "no DRAFT_CONFIG found, exiting..."
+  echo "cihc: no DRAFT_CONFIG found, exiting..."
   exit 1
 fi
 
 if [ ! -f "$DRAFT_CONFIG" ]; then
-    logit "draft haproxy config file $DRAFT_CONFIG does not exist"
+    echo "cihc: draft haproxy config file $DRAFT_CONFIG does not exist"
     exit 1 
 fi
 
 haproxy -c -f "$DRAFT_CONFIG" >/dev/null
 if [ $? -ne 0 ]; then
-    logit "new haproxy config failed, exiting..."
+    echo "cihc: new haproxy config failed, exiting..."
     echo -n "jitsi.haproxy.reconfig_failed:1|c" | nc -4u -w1 localhost 8125
     exit 1
 fi
 
-logit "validated $DRAFT_CONFIG; copying to haproxy.cfg and restarting haproxy"
+echo "cihc: validated $DRAFT_CONFIG; copying to haproxy.cfg and restarting haproxy"
 
 cp "$DRAFT_CONFIG" /etc/haproxy/haproxy.cfg
 service haproxy reload
--- ansible/roles/hcv-haproxy-configure/files/update-haproxy-map.sh
+++ ansible/roles/hcv-haproxy-configure/files/update-haproxy-map.sh
@@ -2,27 +2,23 @@
 #
 # read a haproxy map file and uses haproxy admin socket to update the live configuration
 
-function logit() {
-  echo "## $1"
-  logger -p local0.notice -t ${0##*/}[$$] "$1"
-}
-logit "starting update-haproxy-map.sh"
+echo "starting update-haproxy-map.sh"
 
 if [ -n "$1" ]; then
     UPDATE_MAP=$1
 fi
 
 if [ -z "$UPDATE_MAP" ]; then
-  logit "no UPDATE_MAP found, exiting..."
+  echo "uhm: no UPDATE_MAP found, exiting..."
   exit 1
 fi
 
 if [ ! -f "$UPDATE_MAP" ]; then
-    logit "map file $UPDATE_MAP does not exist"
+    echo "uhm: map file $UPDATE_MAP does not exist"
     exit 1
 fi
 
-logit "updating live config with $UPDATE_MAP"
+echo "uhm: updating live config with $UPDATE_MAP"
 
 PREPARE_VERSION=$(echo "prepare map $UPDATE_MAP" | socat /var/run/haproxy/admin.sock stdio | cut -d ':' -f2 | xargs)
 
@@ -34,7 +30,7 @@ done < "${UPDATE_MAP}"
 
 echo "commit map @$PREPARE_VERSION $UPDATE_MAP" | socat /var/run/haproxy/admin.sock stdio
 if [ $? -ne 0 ]; then
-    logit "commit map failed for $UPDATE_MAP"
+    echo "uhm: commit map failed for $UPDATE_MAP"
     echo -n "jitsi.haproxy.map_update_failed:1|c" | nc -4u -w1 localhost 8125
     exit 1
 fi
