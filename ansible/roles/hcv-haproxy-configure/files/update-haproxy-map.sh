8dcf7bee3f38774f62043b9030c308e1004760fd
scott boone <scott.e.boone@gmail.com>
2023-04-24 11:12:03 -0700

    various fixes

--- ansible/roles/hcv-haproxy-configure/files/update-haproxy-map.sh
+++ ansible/roles/hcv-haproxy-configure/files/update-haproxy-map.sh
@@ -2,9 +2,9 @@
 #
 # read a haproxy map file and uses haproxy admin socket to update the live configuration
 
-logit(msg) {
-  echo "## $MSG"
-  logger -p local0.notice -t ${0##*/}[$$] "$MSG"
+function logit() {
+  echo "## $1"
+  logger -p local0.notice -t ${0##*/}[$$] "$1"
 }
 
 if [ -n "$1" ]; then
@@ -17,11 +17,11 @@ if [ -z "$UPDATE_MAP" ]; then
 fi
 
 if [ ! -f "$UPDATE_MAP" ]; then
-    logit "## update-haproxy-map: map file $UPDATE_MAP does not exist"
+    logit "update-haproxy-map: map file $UPDATE_MAP does not exist"
     exit 1
 fi
 
-logit "## updating live config with $UPDATE_MAP"
+logit "updating live config with $UPDATE_MAP"
 
 PREPARE_VERSION=$(echo "prepare map $UPDATE_MAP" | socat /var/run/haproxy/admin.sock stdio | cut -d ':' -f2 | xargs)
 
@@ -32,7 +32,7 @@ while IFS='' read -r line || [ -n "$line" ]; do
 done < "${UPDATE_MAP}"
 
 echo "commit map @$PREPARE_VERSION $UPDATE_MAP" | socat /var/run/haproxy/admin.sock stdio
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
     logit "commit map failed for $UPDATE_MAP"
     echo -n "jitsi.haproxy.map_update_failed:1|c" | nc -4u -w1 localhost 8125
     exit 1
