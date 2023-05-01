d89eee173f5ede6d0d4c8b3dcdbc769ddc825374
Scott Boone <scott.boone@8x8.com>
2023-05-01 15:05:05 -0500

    save copy of new haproxy config

--- ansible/roles/hcv-haproxy-configure/files/check-install-haproxy-config.sh
+++ ansible/roles/hcv-haproxy-configure/files/check-install-haproxy-config.sh
@@ -2,13 +2,20 @@
 #
 # check a draft haproxy config and install if it's valid
 
-[ -z "$TEMPLATE_LOGFILE" ] && TEMPLATE_LOGFILE="/tmp/template.log"
+[ -z "$TEMPLATE_LOGDIR" ] && TEMPLATE_LOGDIR="/tmp/consul-template"
+[ -z "$TEMPLATE_LOGFILE" ] && TEMPLATE_LOGFILE="$TEMPLATE_LOGDIR/template.log"
+
+if [ ! -f "$TEMPLATE_LOGDIR" ]; then
+  touch $TEMPLATE_LOGDIR
+fi
 
 if [ ! -f "$TEMPLATE_LOGFILE" ]; then
   touch $TEMPLATE_LOGFILE
 fi
 
-echo "$(date --utc +%Y-%m-%d_%H:%M:%S.Z) starting check-install-haproxy-config.sh" >> $TEMPLATE_LOGFILE
+TIMESTAMP=$(date --utc +%Y-%m-%d_%H:%M:%S.Z)
+
+echo "$TIMESTAMP starting check-install-haproxy-config.sh" >> $TEMPLATE_LOGFILE
 
 if [ -n "$1" ]; then
     DRAFT_CONFIG=$1
@@ -33,9 +40,23 @@ else
     echo -n "jitsi.haproxy.reconfig_failed:0|c" | nc -4u -w1 localhost 8125
 fi
 
-echo "#### cihc: validated $DRAFT_CONFIG; copying to haproxy.cfg and restarting haproxy" >> $TEMPLATE_LOGFILE
+echo "#### cihc: validated $DRAFT_CONFIG; copy to haproxy.cfg and reloading haproxy" >> $TEMPLATE_LOGFILE
 
+FINAL_EXIT=0
 cp "$DRAFT_CONFIG" /etc/haproxy/haproxy.cfg
+if [ $? -gt 0 ]; then
+    echo "#### chic: failed to copy the new haproxy config file" >> $TEMPLATE_LOGFILE
+    FINAL_EXIT=1
+fi
+
+# save a copy of the new config
+cp "$DRAFT_CONFIG" $TEMPLATE_LOGDIR/$TIMESTAMP-haproxy.cfg
+
 service haproxy reload
+if [ $? -gt 0 ]; then
+    echo "#### chic: failed to reload haproxy service" >> $TEMPLATE_LOGFILE
+fi
 
 echo -n "jitsi.haproxy.reconfig:1|c" | nc -4u -w1 localhost 8125
+
+echo "#### chic: succeeded to reload haproxy with new config" >> $TEMPLATE_LOGFILE
--- ansible/roles/hcv-haproxy-configure/files/update-haproxy-map.sh
+++ ansible/roles/hcv-haproxy-configure/files/update-haproxy-map.sh
@@ -2,7 +2,12 @@
 #
 # read a haproxy map file and uses haproxy admin socket to update the live configuration
 
-[ -z "$TEMPLATE_LOGFILE" ] && TEMPLATE_LOGFILE="/tmp/template.log"
+[ -z "$TEMPLATE_LOGDIR" ] && TEMPLATE_LOGDIR="/tmp/consul-template"
+[ -z "$TEMPLATE_LOGFILE" ] && TEMPLATE_LOGFILE="$TEMPLATE_LOGDIR/template.log"
+
+if [ ! -f "$TEMPLATE_LOGDIR" ]; then
+  touch $TEMPLATE_LOGDIR
+fi
 
 if [ ! -f "$TEMPLATE_LOGFILE" ]; then
   touch $TEMPLATE_LOGFILE
@@ -41,6 +46,9 @@ if [ $? -gt 0 ]; then
     exit 1
 else
     echo -n "jitsi.haproxy.map_update_failed:1|0" | nc -4u -w1 localhost 8125
+    echo "#### uhm: failed to update map $UPDATE_MAP" >> $TEMPLATE_LOGFILE
 fi
 
 echo -n "jitsi.haproxy.map_update:1|c" | nc -4u -w1 localhost 8125
+
+echo "#### uhm: succeeded to update $UPDATE_MAP" >> $TEMPLATE_LOGFILE
