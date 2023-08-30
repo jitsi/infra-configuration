7548200f9196aae69f1241bf90f943a3e7c877f4
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-08-30 15:15:51 -0400

    attempt to archive tcpdump from jigasi

--- ansible/roles/tcpdump-jigasi/files/tcpdump-jigasi-cleanup.sh
+++ ansible/roles/tcpdump-jigasi/files/tcpdump-jigasi-cleanup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+. /usr/local/bin/oracle_cache.sh
+[ -z "$DUMP_BUCKET_NAME" ] && DUMP_BUCKET_NAME="stats-${ENVIRONMENT}"
+JHOST="$(hostname -s)"
 
-/usr/bin/find /var/lib/tcpdump-jigasi -type f -mmin +300 -exec rm {} \;
+PATHS="$(/usr/bin/find /var/lib/tcpdump-jigasi -type f -mmin +300)"
+for dumpfile in $PATHS; do
+    DUMP_PATH="tcpdump-jigasi/jigasi-release-${JIGASI_RELEASE_NUMBER}/${JHOST}/$(basename $dumpfile)"
+    $OCI_BIN os object put --force -bn "$DUMP_BUCKET_NAME" --name "$DUMP_PATH" --file "$dumpfile" --metadata '{"environment":"'"$ENVIRONMENT"'","jigasi-release-number":"'"$JIGASI_RELEASE_NUMBER"'"}' --region "$ORACLE_REGION" --auth instance_principal
+
+    /bin/rm -f $dumpfile
+done
