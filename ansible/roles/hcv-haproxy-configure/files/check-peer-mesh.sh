d8f033ce86a306f360dcf0e7f388fe72e35e43cd
scott boone <scott.e.boone@gmail.com>
2023-10-12 18:46:42 -0500

    missed situation where there was no match

--- ansible/roles/hcv-haproxy-configure/files/check-peer-mesh.sh
+++ ansible/roles/hcv-haproxy-configure/files/check-peer-mesh.sh
@@ -14,15 +14,15 @@ echo "#### cpm: $TIMESTAMP starting check-peer-mesh.sh" >> $LOGFILE
 # check to make sure the haproxy has at least 1 remote peer
 
 REMOTE_PEER_DATA=$(echo "show peers" | sudo -u haproxy socat stdio /var/run/haproxy/admin.sock | grep haproxy | grep remote)
-REMOTE_PEER_COUNT=$(echo "$REMOTE_PEER_DATA" | wc | awk -F" " '{print $1}')
 
-if [ "$REMOTE_PEER_COUNT" -eq 0 ]; then
+if [ "$?" -ne 0 ]; then
     echo "#### cpm: haproxy has no remote peers" >> $LOGFILE
     exit 1
 fi
 
 # check to make sure that all haproxy peers have established connections
 
+REMOTE_PEER_COUNT=$(echo "$REMOTE_PEER_DATA" | wc | awk -F" " '{print $1}')
 REMOTE_PEER_ESTABLISHED_COUNT=$(echo "$REMOTE_PEER_DATA" | grep ESTA | wc | awk -F" " '{print $1}')
 
 if [ "$REMOTE_PEER_ESTABLISHED_COUNT" -ne "$REMOTE_PEER_COUNT" ]; then
