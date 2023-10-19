e707096e8a5974aae0f5deaa5561706d890cdd1f
scott boone <scott.e.boone@gmail.com>
2023-10-19 08:45:09 -0500

    verbose

--- ansible/roles/hcv-haproxy-configure/files/oci-lb-backend-drain.sh
+++ ansible/roles/hcv-haproxy-configure/files/oci-lb-backend-drain.sh
@@ -21,7 +21,7 @@ fi
 
 [ -z "$DRAIN_STATE" ] && DRAIN_STATE="true"
 
-echo "#### olbd: instance pool is $INSTANCE_POOL" >> $LOGFILE
+echo "#### olbd: instance pool is $INSTANCE_POOL in region $ORACLE_REGION" >> $LOGFILE
 
 # get instance pool details with oci cli
 INSTANCE_POOL_DETAILS="$(oci compute-management instance-pool get --instance-pool-id $INSTANCE_POOL --region $ORACLE_REGION --auth instance_principal)"
