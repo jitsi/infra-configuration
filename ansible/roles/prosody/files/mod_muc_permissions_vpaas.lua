1024f81e10b84ce9658b2582dae9a38759099d99
damencho <damencho@jitsi.org>
2024-01-31 08:16:19 -0600

    feat: Adds more logs for vpass disabled features.

--- ansible/roles/prosody/files/mod_muc_permissions_vpaas.lua
+++ ansible/roles/prosody/files/mod_muc_permissions_vpaas.lua
@@ -30,25 +30,30 @@ module:hook("muc-room-pre-create", function(event)
         return;
     end
 
+    local meeting_fqn, customer_id = util.get_fqn_and_customer_id(room);
+
     local function cb(content_, code_, _, _)
         local room = room
         if code_ == 200 then
             local jaas_actuator_res = json.decode(content_);
             module:log("debug", "Receive jaas actuator response %s", inspect(jaas_actuator_res))
             room._data.disabled_features = jaas_actuator_res.disabledFeatures;
+            if room._data.disabled_features then
+                module:log("info", "disabled features: %s for customer_id: %s", inspect(room._data.disabled_features), customer_id);
+            end
+
             if jaas_actuator_res.status ~= nil and (jaas_actuator_res.status == "BLOCKED" or jaas_actuator_res.status == "DELETED") then
                 room._data.disabled_access = true;
             else
                 room._data.disabled_access = false;
             end
         else
-            module:log("debug", "External call to jaas-actuator failed, we do not set any disabled features")
+            module:log("warn", "External call to jaas-actuator failed, we do not set any disabled features")
             room._data.disabled_access = false;
             room._data.disabled_features = {};
         end
     end
 
-    local meeting_fqn, customer_id = util.get_fqn_and_customer_id(room);
     local jaas_actuator_customer_details_url = jaas_actuator_base_url .. "/v1/customers/" .. customer_id;
 
     local headers = http_headers or {}
