c49b0a2cfae7157e7f913f4f11d05545c41c63a0
Aaron van Meerten <aaron.van.meerten@gmail.com>
2024-04-19 14:48:01 -0500

    task: notify autoscaler-sidecar of imminent shutdown (#469)
    * task: notify autoscaler-sidecar of imminent shutdown
    
    * ensure curl bin is set

--- ansible/roles/autoscaler-sidecar/files/terminate_instance_oracle.sh
+++ ansible/roles/autoscaler-sidecar/files/terminate_instance_oracle.sh
@@ -4,6 +4,10 @@ export OCI_BIN="/usr/local/bin/oci"
 export CURL_BIN="/usr/bin/curl"
 export JQ_BIN="/usr/bin/jq"
 
+# notify the sidecar of imminent shutdown
+$CURL_BIN -d '{}' -v localhost:6000/hook/v1/shutdown
+sleep 10
+
 if [ -z "$INSTANCE_ID" ]; then
     INSTANCE_ID=$($CURL_BIN -s http://169.254.169.254/opc/v1/instance/ | $JQ_BIN .id -r)
 fi
