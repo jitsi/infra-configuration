384edab0c3a18b4782babef1ff3dba0a2aeb199e
scott boone <sawall@users.noreply.github.com>
2024-09-04 11:21:03 -0500

    improved log messages for haproxy consul-template script (#573)

--- ansible/roles/hcv-haproxy-configure/files/haproxy-configurator-payload.sh
+++ ansible/roles/hcv-haproxy-configure/files/haproxy-configurator-payload.sh
@@ -7,68 +7,80 @@ echo -n "jitsi.haproxy.reconfig_locked:1|c" | nc -4u -w1 localhost 8125
 if [ -n "$1" ]; then
     TEMPLATE_LOGFILE=$1
 else
-    echo "## hc: missing TEMPLATE_LOGFILE, exiting"
+    echo "hap-cfg-payload: missing TEMPLATE_LOGFILE, exiting"
     exit 1
 fi
 
-echo "#### hcp: entered haproxy-configurator-payload.sh" >> $TEMPLATE_LOGFILE
+function timestamp() {
+  echo $(date --utc +%Y-%m-%d_%H:%M:%S.Z)
+}
+
+function log_msg() {
+  echo "$(timestamp) [$$] hap-cfg-payload: $1" | tee -a $TEMPLATE_LOGFILE
+}
+
+log_msg "entered haproxy-configurator-payload.sh"
 
 if [ -n "$2" ]; then
     DRAFT_CONFIG_VALIDATED=$2
 else
-    echo "## hc: missing DRAFT_CONFIG_VALIDATED, exiting" >> $TEMPLATE_LOGFILE
+    log_msg "missing DRAFT_CONFIG_VALIDATED, exiting"
     FINAL_EXIT=1
 fi
 
 diff $DRAFT_CONFIG_VALIDATED /etc/haproxy/haproxy.cfg
 if [ $? -eq 0 ]; then
-    echo "#### hcp: the validated draft config is identical to the installed config; skipping" >> $TEMPLATE_LOGFILE
+    log_msg "the validated draft config is identical to the installed config; skipping"
     exit 0
 fi
 
 # copy the validated config to the real config
 cp $DRAFT_CONFIG_VALIDATED /etc/haproxy/haproxy.cfg
 
-echo "#### hcp: draining load balancer" | tee -a $TEMPLATE_LOGFILE
+log_msg "draining from load balancer"
 /usr/local/bin/oci-lb-backend-drain.sh $TEMPLATE_LOGFILE
 if [ $? -gt 0 ]; then
-    echo "#### hcp: haproxy failed to drain from the load balancer" | tee -a $TEMPLATE_LOGFILE
+    log_msg "haproxy failed to drain from the load balancer"
     FINAL_EXIT=1
 fi
 
 if [[ "$FINAL_EXIT" -eq 0 ]]; then
-    echo "#### hcp: reloading haproxy" >> $TEMPLATE_LOGFILE
+    log_msg "reloading haproxy"
 
     # make sure the most recent validated config is being used
     cp $DRAFT_CONFIG_VALIDATED /etc/haproxy/haproxy.cfg
 
     service haproxy reload
-
     if [[ $? -gt 0 ]]; then
-        echo "#### hcp: haproxy failed to reload" | tee -a $TEMPLATE_LOGFILE
+        log_Msg "haproxy failed to reload"
         echo -n "jitsi.haproxy.reconfig:0|c" | nc -4u -w1 localhost 8125
         FINAL_EXIT=1
+    else
+        echo -n "jitsi.haproxy.reconfig:1|c" | nc -4u -w1 localhost 8125
     fi
-    echo -n "jitsi.haproxy.reconfig:1|c" | nc -4u -w1 localhost 8125
 fi
 
 # undrain the haproxy from the load balancer
-echo "#### hcp: undraining load balancer" | tee -a $TEMPLATE_LOGFILE
+log_msg "undraining from load balancer"
 DRAIN_STATE="false" /usr/local/bin/oci-lb-backend-drain.sh $TEMPLATE_LOGFILE
 if [[ $? -gt 0 ]] && [[ "$FINAL_EXIT" -eq 0 ]]; then
-    echo "#### hcp: haproxy failed to undrain from the load balancer" | tee -a $TEMPLATE_LOGFILE
+    log_msg "failed to undrain the load balancer"
     FINAL_EXIT=1
 fi
 
 # log that a reconfigure happened
-echo "#### hcp: succeeded to reload haproxy with new config" >> $TEMPLATE_LOGFILE
+log_msg "reloaded haproxy with new config"
 
 # copy the live config over test config and kick off consul-template to make sure there are not any new changes
 diff /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.test
 if [ $? -ne 0 ]; then
-    echo "#### hcp: live config is different than test config; re-running" >> $TEMPLATE_LOGFILE
+    log_msg "live config is different than test config; reloading consul-template"
     cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.test
     service consul-template reload
+    if [[ $? -gt 0 ]]; then
+        log_Msg "consul-template failed to reload"
+        FINAL_EXIT=1
+    fi
 fi
 
 if [[ $FINAL_EXIT -gt 0 ]]; then
