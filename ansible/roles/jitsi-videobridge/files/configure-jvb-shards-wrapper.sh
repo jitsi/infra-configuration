e508152d3a46e51fef7044d84d3057418414b628
scott boone <scott.e.boone@gmail.com>
2024-09-27 12:19:45 -0500

    improve namespace and add a missed log

--- ansible/roles/jitsi-videobridge/files/configure-jvb-shards-wrapper.sh
+++ ansible/roles/jitsi-videobridge/files/configure-jvb-shards-wrapper.sh
@@ -14,18 +14,18 @@ fi
 cp /etc/jitsi/videobridge/shards.json /var/log/jitsi/jvb-shards/$(date --utc +%Y-%m-%d_%H:%M:%S.Z)-shards.json
 
 echo "$(date --utc +%Y-%m-%d_%H:%M:%S.Z) starting configure-jvb-shards.sh" >> $TEMPLATE_LOGFILE
-/usr/local/bin/configure-jvb-shards.sh
+/usr/local/bin/configure-jvb-shards.sh >> $TEMPLATE_LOGFILE
 RET=$?
 
 if [ $? -gt 0 ]; then
     echo "$(date --utc +%Y-%m-%d_%H:%M:%S.Z) jvb shards update failed" >> $TEMPLATE_LOGFILE
-    echo -n "jitsi.jvb.shards_update_update_failed:1|c" | nc -4u -w1 localhost 8125
+    echo -n "jitsi.config.jvb.shards_update_update_failed:1|c" | nc -4u -w1 localhost 8125
     exit 1
 else
-    echo -n "jitsi.jvb.shards_update_failed:0|c" | nc -4u -w1 localhost 8125
+    echo -n "jitsi.config.jvb.shards_update_failed:0|c" | nc -4u -w1 localhost 8125
 fi
 
-echo -n "jitsi.jvb.shards_update:1|c" | nc -4u -w1 localhost 8125
+echo -n "jitsi.config.jvb.shards_update:1|c" | nc -4u -w1 localhost 8125
 
 echo "$(date --utc +%Y-%m-%d_%H:%M:%S.Z) jvb shards reconfiguration complete" >> $TEMPLATE_LOGFILE
 
