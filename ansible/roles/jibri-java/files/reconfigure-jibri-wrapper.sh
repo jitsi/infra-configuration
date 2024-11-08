086d8f93b4ebb3d8baa3699eb44786f1df4ee718
Aaron van Meerten <aaron.van.meerten@8x8.com>
2024-11-08 10:37:28 -0600

    update log method

--- ansible/roles/jibri-java/files/reconfigure-jibri-wrapper.sh
+++ ansible/roles/jibri-java/files/reconfigure-jibri-wrapper.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
+function timestamp() {
+  date --utc +%Y-%m-%d_%H:%M:%S.Z
+}
+
+function log_msg() {
+  echo "$(timestamp) [$$] jibri-wrapper: $1" | tee -a $TEMPLATE_LOGFILE
+}
+
 [ -z "$TEMPLATE_LOGDIR" ] && TEMPLATE_LOGDIR="/var/log/jitsi/jibri-shards"
 
 [ -d "$TEMPLATE_LOGDIR" ] || mkdir -p $TEMPLATE_LOGDIR
 [ -z "$TEMPLATE_LOGFILE" ] && TEMPLATE_LOGFILE="$TEMPLATE_LOGDIR/jibri-reconfigure.log"
 [ -z "$XMPP_CONF_FILE" ] && XMPP_CONF_FILE="/etc/jitsi/jibri/xmpp.conf"
 
-echo "$(date --utc +%Y-%m-%d_%H:%M:%S.Z) starting reconfigure-jibri-wrapper.sh" >> $TEMPLATE_LOGFILE
+log_msg "starting"
 cp $XMPP_CONF_FILE $TEMPLATE_LOGDIR/xmpp.conf.$(date --utc +%Y-%m-%d_%H:%M:%S.Z)
 
 /usr/sbin/service jibri reload
 RET=$?
 
 if [ $RET -gt 0 ]; then
-    echo "$(date --utc +%Y-%m-%d_%H:%M:%S.Z) jibri shards update failed" >> $TEMPLATE_LOGFILE
+    log_msg "update failed"
     echo -n "jitsi.config.jibri.shards_update_update_failed:1|c" | nc -4u -w1 localhost 8125
 else
-    echo "$(date --utc +%Y-%m-%d_%H:%M:%S.Z) jibri shards update successful" >> $TEMPLATE_LOGFILE
+    log_msg "update successful"
     echo -n "jitsi.config.jibri.shards_update_failed:0|c" | nc -4u -w1 localhost 8125
 fi
 
 echo -n "jitsi.config.jibri.shards_update:1|c" | nc -4u -w1 localhost 8125
 
-echo "$(date --utc +%Y-%m-%d_%H:%M:%S.Z) jibri shards reconfiguration sleep 10 after reload" >> $TEMPLATE_LOGFILE
+log_msg "sleep 10 after reload"
 sleep 10
 
-echo "$(date --utc +%Y-%m-%d_%H:%M:%S.Z) jibri shards reconfiguration complete" >> $TEMPLATE_LOGFILE
+log_msg "complete"
 
 
 exit $RET
