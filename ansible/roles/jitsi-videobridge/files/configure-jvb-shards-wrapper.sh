256f04ba3e07ff8b375a627473b4ca968f0fe767
Aaron van Meerten <aaron.van.meerten@8x8.com>
2024-12-18 15:05:37 -0600

    feat(jvb): consul reconfiguration only after 10 seconds

--- ansible/roles/jitsi-videobridge/files/configure-jvb-shards-wrapper.sh
+++ ansible/roles/jitsi-videobridge/files/configure-jvb-shards-wrapper.sh
@@ -3,6 +3,37 @@
 [ -z "$TEMPLATE_LOGDIR" ] && TEMPLATE_LOGDIR="/var/log/jitsi/jvb-shards"
 [ -z "$TEMPLATE_LOGFILE" ] && TEMPLATE_LOGFILE="$TEMPLATE_LOGDIR/jvb-reconfigure.log"
 
+CONSUL_TEMPLATE_SHARDS_JSON="/etc/jitsi/videobridge/shards-candidate.json"
+DRAFT_CONFIG_VALIDATED="/etc/jitsi/videobridge/shards-validated.json"
+
+function timestamp() {
+  echo $(date --utc +%Y-%m-%d_%H:%M:%S.Z)
+}
+
+function log_msg() {
+  echo "$(timestamp) [$$] jvb-check-cfg: $1" | tee -a $TEMPLATE_LOGFILE
+}
+
+log_msg "starting configure-jvb-shards-wrapper.sh"
+
+readonly PROGNAME=$(basename "$0")
+readonly LOCKFILE_DIR=/tmp
+readonly LOCK_FD=200
+
+function lock() {
+    local prefix=$1
+    local fd=${2:-$LOCK_FD}
+    local lock_file=$LOCKFILE_DIR/$prefix.lock
+
+    # create lock file
+    eval "exec $fd>$lock_file"
+
+    # aquire the lock
+    flock -n $fd \
+        && return 0 \
+        || return 1
+}
+
 if [ ! -d "$TEMPLATE_LOGDIR" ]; then
   mkdir $TEMPLATE_LOGDIR
 fi
@@ -11,22 +42,33 @@ if [ ! -f "$TEMPLATE_LOGFILE" ]; then
   touch $TEMPLATE_LOGFILE
 fi
 
-cp /etc/jitsi/videobridge/shards.json /var/log/jitsi/jvb-shards/$(date --utc +%Y-%m-%d_%H:%M:%S.Z)-shards.json
+# always emit at least a 0 to metrics
+echo -n "jitsi.config.jvb.reconfig:0|c" | nc -4u -w1 localhost 8125
 
-echo "$(date --utc +%Y-%m-%d_%H:%M:%S.Z) starting configure-jvb-shards.sh" >> $TEMPLATE_LOGFILE
-/usr/local/bin/configure-jvb-shards.sh >> $TEMPLATE_LOGFILE
-RET=$?
+CONFIG_TIMESTAMP=$(timestamp)
 
-if [ $RET -gt 0 ]; then
-    echo "$(date --utc +%Y-%m-%d_%H:%M:%S.Z) jvb shards update failed" >> $TEMPLATE_LOGFILE
-    echo -n "jitsi.config.jvb.shards_update_update_failed:1|c" | nc -4u -w1 localhost 8125
+# validate the draft configuration
+jq "$CONSUL_TEMPLATE_SHARDS_JSON" >/dev/null
+if [ $? -gt 0 ]; then
+    log_msg "new JVB shards json failed to validate"
+    echo -n "jitsi.config.jvb.reconfig.failed:1|c" | nc -4u -w1 localhost 8125
+    # log a copy of the new config
+    cp "$CONSUL_TEMPLATE_SHARDS_JSON" $TEMPLATE_LOGDIR/$CONFIG_TIMESTAMP-shards.json.invalid
     exit 1
 else
-    echo -n "jitsi.config.jvb.shards_update_failed:0|c" | nc -4u -w1 localhost 8125
+    cp $CONSUL_TEMPLATE_SHARDS_JSON $DRAFT_CONFIG_VALIDATED
+    cp /etc/jitsi/videobridge/shards.json $TEMPLATE_LOGDIR/$CONFIG_TIMESTAMP-shards.json
+    log_msg "validated $CONSUL_TEMPLATE_SHARDS_JSON"
+    echo -n "jitsi.config.jvb.reconfig.failed:0|c" | nc -4u -w1 localhost 8125
 fi
 
-echo -n "jitsi.config.jvb.shards_update:1|c" | nc -4u -w1 localhost 8125
+lock $PROGNAME
 
-echo "$(date --utc +%Y-%m-%d_%H:%M:%S.Z) jvb shards reconfiguration complete" >> $TEMPLATE_LOGFILE
-
-exit $RET
+if [[ "$?" -eq 0 ]]; then
+    /usr/local/bin/jvb-configurator.sh $TEMPLATE_LOGFILE $DRAFT_CONFIG_VALIDATED &
+    log_msg "jvb-configurator.sh forked"
+    echo -n "jitsi.config.jvb.configurator:1|c" | nc -4u -w1 localhost 8125
+else
+    log_msg "jvb-configurator.sh not started; there is already a fork running"
+    echo -n "jitsi.config.jvb.configurator:0|c" | nc -4u -w1 localhost 8125
+fi
---
+++ ansible/roles/jitsi-videobridge/files/jvb-configurator.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+# jvb-configurator fork
+
+if [ -n "$1" ]; then
+    TEMPLATE_LOGFILE=$1
+else
+    echo "jvb-configurator: missing TEMPLATE_LOGFILE, exiting"
+    exit 1
+fi
+
+CONSUL_TEMPLATE_SHARDS_JSON="/etc/jitsi/videobridge/shards-candidate.json"
+LIVE_SHARD_JSON="/etc/jitsi/videobridge/shards.json"
+
+function timestamp() {
+  echo $(date --utc +%Y-%m-%d_%H:%M:%S.Z)
+}
+
+function log_msg() {
+  echo "$(timestamp) [$$] jvb-cfg: $1" | tee -a $TEMPLATE_LOGFILE
+}
+
+log_msg "entered jvb-configurator.sh"
+
+FINAL_EXIT=0
+
+if [ -n "$2" ]; then
+    DRAFT_CONFIG_VALIDATED=$2
+else
+    log_msg "missing DRAFT_CONFIG_VALIDATED, exiting"
+    FINAL_EXIT=1
+fi
+
+if [ ! -f "$DRAFT_CONFIG_VALIDATED" ] && [ "$FINAL_EXIT" == "0" ]; then
+    log_msg "draft jvb shards file $DRAFT_CONFIG_VALIDATED does not exist, exiting"
+    exit 1
+fi
+
+diff $DRAFT_CONFIG_VALIDATED $LIVE_SHARD_JSON
+if [ $? -eq 0 ]; then
+    log_msg "$DRAFT_CONFIG_VALIDATED is identical to $LIVE_SHARD_JSON, exiting configurator"
+    exit 0
+fi
+
+if [ "$FINAL_EXIT" == "0" ]; then
+    log_msg "waiting 10 seconds for the validated configuration file to stabilize..."
+    while true; do
+        UNIX_TIME_OF_VALIDATED_CONFIG_FILE=$(stat -c %Y $DRAFT_CONFIG_VALIDATED)
+        UNIX_TIME=$(date +%s)
+        if (( $UNIX_TIME > $UNIX_TIME_OF_VALIDATED_CONFIG_FILE + 10 )); then
+            log_msg "validated shards json file has been stable for 10 seconds, initiating processing"
+            cp $DRAFT_CONFIG_VALIDATED $LIVE_SHARD_JSON
+            /usr/local/bin/configure-jvb-shards.sh | tee -a $TEMPLATE_LOGFILE
+            RET=$?
+
+            if [ $RET -gt 0 ]; then
+                echo "$(date --utc +%Y-%m-%d_%H:%M:%S.Z) jvb shards update failed" >> $TEMPLATE_LOGFILE
+                echo -n "jitsi.config.jvb.shards_update_update_failed:1|c" | nc -4u -w1 localhost 8125
+                exit 1
+            else
+                echo -n "jitsi.config.jvb.shards_update_failed:0|c" | nc -4u -w1 localhost 8125
+            fi
+            # copy the live config over test config and kick off consul-template to make sure there are not any new changes
+            diff $LIVE_SHARD_JSON $CONSUL_TEMPLATE_SHARDS_JSON
+            if [ $? -ne 0 ]; then
+                log_msg "live config is different than test config; reloading consul-template"
+                cp $LIVE_SHARD_JSON $CONSUL_TEMPLATE_SHARDS_JSON
+                service consul-template reload
+                if [[ $? -gt 0 ]]; then
+                    log_msg "consul-template failed to reload"
+                    FINAL_EXIT=1
+                fi
+            fi
+            break
+        else
+            sleep 1
+        fi
+    done
+fi
+
+echo -n "jitsi.config.jvb.shards_update:1|c" | nc -4u -w1 localhost 8125
+echo "$(date --utc +%Y-%m-%d_%H:%M:%S.Z) jvb shards reconfiguration complete" >> $TEMPLATE_LOGFILE
+
+exit $FINAL_EXIT
