c1ec7ac5a29a844402d3aad6d4a8e92654910b10
Aaron van Meerten <aaron.van.meerten@8x8.com>
2025-02-21 13:40:16 -0600

    fix vo agent config better handling of upload failures

--- ansible/roles/jibri-docker-compose/files/custom-start.sh
+++ ansible/roles/jibri-docker-compose/files/custom-start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cp /usr/local/bin/*.jar /usr/bin
 cp /etc/jitsi/jibri/* /config
+cp /opt/vo-meet-agent/vo-meet-agent-config.yml /etc/vo-meet-agent/vo-meet-agent-config.yml
+chown -R jibri:jibri /etc/vo-meet-agent/vo-meet-agent-config.yml
