a3bb2babfe506a2fa8d641a7bbd6e546fe65a97b
Aaron van Meerten <aaron.van.meerten@8x8.com>
2025-02-21 13:52:58 -0600

    update custom start

--- ansible/roles/jibri-docker-compose/files/custom-start.sh
+++ ansible/roles/jibri-docker-compose/files/custom-start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp /usr/local/bin/*.jar /usr/bin
 cp /etc/jitsi/jibri/* /config
+mkdir -p /etc/vo-meet-agent
 cp /opt/vo-meet-agent/vo-meet-agent-config.yml /etc/vo-meet-agent/vo-meet-agent-config.yml
 chown -R jibri:jibri /etc/vo-meet-agent/vo-meet-agent-config.yml
