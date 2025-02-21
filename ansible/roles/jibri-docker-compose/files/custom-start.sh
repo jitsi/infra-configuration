d5a3a2acda0c36537408a06328524d39a8e456ec
Aaron van Meerten <aaron.van.meerten@8x8.com>
2025-02-21 15:09:54 -0600

    other fixes

--- ansible/roles/jibri-docker-compose/files/custom-start.sh
+++ ansible/roles/jibri-docker-compose/files/custom-start.sh
@@ -5,4 +5,4 @@ cp /usr/local/bin/*.jar /usr/bin
 cp /etc/jitsi/jibri/* /config
 mkdir -p /etc/jitsi
 cp /opt/vo-meet-agent/vo-meet-agent-config.yml /etc/jitsi/vo-meet-agent-config.yml
-chown -R jibri:jibri /etc/vo-meet-agent/vo-meet-agent-config.yml
+chown -R jibri:jibri /etc/jitsi/vo-meet-agent-config.yml
