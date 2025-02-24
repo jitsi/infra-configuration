cd53d18e86a91df48c3c1d41056a6886dc5efc60
Aaron van Meerten <aaron.van.meerten@gmail.com>
2025-02-24 11:19:40 -0600

    feat(jibri): run jibri in docker-compose (#692)
    * feat(jibri): jibri in docker-compose
    
    * continue jibri standalone docker compose
    
    * bucket creds for compose
    
    * sidecar skip task
    
    * shell instead of command
    
    * add -1 to jibri version tag
    
    * docker daemon config for jibri compose
    
    * fix vo agent config
    better handling of upload failures
    
    * include recording upload failure
    
    * update custom start
    
    * set jibri verison fact
    use fact when tagging for wavefront
    
    * fix config script
    
    * fix jibri health and stats
    
    * fix health cron output
    
    * further tuning
    
    * ensure oci-cli inside container
    
    * other fixes
    
    * mount dir for dynamic file content changes on host

---
+++ ansible/roles/jibri-docker-compose/files/custom-start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get -y update && apt-get -y install python3-pip
+pip install oci-cli --break-system-packages
+cp /usr/local/bin/*.jar /usr/bin
+cp /etc/jitsi/jibri/* /config
+mkdir -p /etc/jitsi
+cp /opt/vo-meet-agent/vo-meet-agent-config.yml /etc/jitsi/vo-meet-agent-config.yml
+chown -R jibri:jibri /etc/jitsi/vo-meet-agent-config.yml
