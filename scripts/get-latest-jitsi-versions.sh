5d0c8579ff4d6d733d0f31b2a9bfe887d2110983
Aaron van Meerten <aaron.van.meerten@8x8.com>
2025-03-25 11:36:56 -0500

    feat(jitsi-versions): allow override of target versions

--- scripts/get-latest-jitsi-versions.sh
+++ scripts/get-latest-jitsi-versions.sh
@@ -18,7 +18,8 @@ sudo apt -y install extrepo
 sudo extrepo enable prosody
 sudo apt update
 
-PROSODY_VERSION=$(apt-cache madison prosody | awk '{print $3;}' | head -1 |  cut -d'-' -f1)
+[ -n "$FORCE_PROSODY_VERSION" ] && PROSODY_VERSION=$FORCE_PROSODY_VERSION
+[ -z "$PROSODY_VERSION" ] && export PROSODY_VERSION=$(apt-cache madison prosody | awk '{print $3;}' | head -1 |  cut -d'-' -f1)
 
 # find meta version
 [ -z "$JITSI_MEET_META_VERSION" ] && JITSI_MEET_META_VERSION=$(apt-cache madison jitsi-meet | awk '{print $3;}' | head -1 |  cut -d'-' -f1)
@@ -32,9 +33,9 @@ jitsi_videobridge_min=$(apt-cache madison jitsi-videobridge2 | awk '{print $3;}'
 
 jicofo_min=${jicofo_min#1.0-}
 jitsi_meet_web_min=${jitsi_meet_web_min#1.0.}
-export JITSI_MEET_VERSION=${jitsi_meet_web_min}
-export JVB_VERSION=${jitsi_videobridge_min}
-export JICOFO_VERSION=${jicofo_min}
+[ -z "$JITSI_MEET_VERSION" ] && export JITSI_MEET_VERSION=${jitsi_meet_web_min}
+[ -z "$JVB_VERSION" ] && export JVB_VERSION=${jitsi_videobridge_min}
+[ -z "$JICOFO_VERSION" ] && export JICOFO_VERSION=${jicofo_min}
 
 echo "JVB_VERSION=$JVB_VERSION">> build_versions.properties
 echo "JICOFO_VERSION=$JICOFO_VERSION" >> build_versions.properties
