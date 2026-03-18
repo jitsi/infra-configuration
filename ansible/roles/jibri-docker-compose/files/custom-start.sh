401d158e844251e075e2c1e8010048e835e98c42
Aaron van Meerten <aaron.van.meerten@8x8.com>
2026-03-18 11:51:30 +0100

    remove break system packages everywhere

--- ansible/roles/jibri-docker-compose/files/custom-start.sh
+++ ansible/roles/jibri-docker-compose/files/custom-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt-get -y update && apt-get -y install python3-pip
-pip install oci-cli --break-system-packages
+apt-get -y update && apt-get -y install python3-pip python3-venv
+python3 -m venv /opt/oci-cli-venv && /opt/oci-cli-venv/bin/pip install oci-cli && ln -sf /opt/oci-cli-venv/bin/oci /usr/local/bin/oci
 cp /usr/local/bin/*.jar /usr/bin
 cp /etc/jitsi/jibri/* /config
 mkdir -p /etc/jitsi
